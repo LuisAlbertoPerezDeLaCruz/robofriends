1d571083439c47603b408081665065c208b9d2e9
Luis Alberto Perez <luisalbertoperez@gmail.com>
2023-01-13 06:37:33 -0400

    organization

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "tachyons";
-import App from "./App";
+import App from "./containers/App";
 
 import reportWebVitals from "./reportWebVitals";
 
